195825dec92ddafd95b7f1c0683b140a157ea3a9
jorandmol <jorandmol.dev@gmail.com>
2023-07-02 12:46:07 +0200

    Stablish web appearance
    Set theme, create pages and structure the content

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,18 @@
 <script>
 	import '../app.css';
+	import { page } from '$app/stores';
 </script>
 
-<slot />
+<header class="bg-white flex flex-col gap-4 items-center justify-center sticky top-0">
+	<div class="mt-5">
+		LOL
+	</div>
+	<div class="tabs mb-5">
+		<a href="/" class="tab tab-lg" class:tab-active={$page.url.pathname === '/'}>Home</a> 
+		<a href="/operate" class="tab tab-lg" class:tab-active={$page.url.pathname === '/operate'}>Operate</a>
+	  </div>
+</header>
+
+<main class="flex flex-grow items-center content-center justify-center pb-20">
+	<slot />
+</main>
---
+++ src/routes/operate/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+
+</script>
+
+<h1 class="text-primary font-bold">Operations</h1>
